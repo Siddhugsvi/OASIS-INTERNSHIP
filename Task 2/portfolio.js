91d091f9fe2334b8967b0b7ee67b7a1b0c76f570
Siddhugsvi <88098337+Siddhugsvi@users.noreply.github.com>
2023-08-02 23:06:55 +0530

    Add files via upload

---
+++ Task 2/portfolio.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text", {
+    strings:["Frontend Developer","Digital Marketer","Web Developer"],
+    typespeed:1000,
+    backSpeed:100,
+    backDelay:100,
+    loop:true,
+})
